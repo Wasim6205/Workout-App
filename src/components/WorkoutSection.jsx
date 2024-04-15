ccce899fc5433805fa0f874b1f60fd69f1775386
Wasim <wasimbhai6205@gmail.com>
2024-04-15 12:39:50 +0530

    some changes

--- src/components/WorkoutSection.jsx
+++ src/components/WorkoutSection.jsx
@@ -127,31 +127,7 @@ const WorkoutSection = () => {
         src="/group-1.svg"
       />
       <div className="absolute top-[0px] left-[0px] w-[375px] h-11 overflow-hidden text-center text-mini text-gray-300">
-        <div className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] bg-white hidden" />
-        <div className="absolute top-[16px] right-[14.3px] w-[66.7px] h-[11.7px]">
-          <div className="absolute top-[0px] right-[0.1px] w-[24.3px] h-[11.3px]">
-            <div className="absolute top-[0px] right-[2.3px] rounded-[2.67px] box-border w-[22px] h-[11.3px] opacity-[0.35] mix-blend-normal border-[1px] border-solid border-gray-300" />
-            <img
-              className="absolute top-[3.7px] right-[0px] w-[1.3px] h-1 opacity-[0.4] mix-blend-normal"
-              alt=""
-              src="/cap.svg"
-            />
-            <div className="absolute top-[2px] right-[4.3px] rounded-[1.33px] bg-gray-300 w-[18px] h-[7.3px]" />
-          </div>
-          <img
-            className="relative w-[15.3px] h-[11px]"
-            alt=""
-            src="/wifi.svg"
-          />
-          <img
-            className="relative w-[17px] h-[10.7px]"
-            alt=""
-            src="/cellular-connection.svg"
-          />
-        </div>
-        <div className="absolute w-[14.4%] top-[calc(50%_-_10px)] left-[3.73%] tracking-[-0.3px] font-semibold inline-block">
-          9:41
-        </div>
+        <div className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] bg-white hidden" />     
       </div>
     </div>
   )
